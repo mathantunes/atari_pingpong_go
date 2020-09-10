6d86bee291fe5da16c8bbb88aeb6682562937b48
unknown <matheus.antunes.96@gmail.com>
2020-09-09 23:06:13 -0300

    ™ DOMAIN: Define internal key values

--- domain/valueobjects.go
+++ domain/valueobjects.go
@@ -1,6 +1,14 @@
 package domain
 
+// KeyboardEvent Representation of a user input on a keyboard
 type KeyboardEvent struct {
-	Keydown bool
-	Key     int
+	Keydown uint8
+	Key     Key
 }
+
+type Key int
+
+const (
+	ArrowUp Key = iota
+	ArrowDown
+)
