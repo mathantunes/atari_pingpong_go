be216767fc19394d381a3af80e06a9187ab6ccec
unknown <matheus.antunes.96@gmail.com>
2020-09-13 11:56:12 -0300

    ™ DOMAIN: Refactor of int to float32 and comment on methods

--- domain/entities.go
+++ domain/entities.go
@@ -3,37 +3,40 @@ package domain
 // Ball Represents the ping pong ball
 type Ball struct {
 	pos    Position
-	radius int
+	radius float32
 	vel    Velocity
 	color  Color
 }
 
+// Draw ball
 func (b *Ball) Draw(pixels []byte) {
 	// Iterate over a square, in each pixel, we check if inside or outside the radius
 	for y := -b.radius; y < b.radius; y++ { //Iterate on Y first for best memory management
 		for x := -b.radius; x < b.radius; x++ {
 			if x*x+y*y < b.radius*b.radius {
-				setPixels(int(b.pos.x)+x, int(b.pos.y)+y, b.color, pixels)
+				setPixels(int(b.pos.x+x), int(b.pos.y+y), b.color, pixels)
 			}
 		}
 	}
 }
 
+// Update ball position and velocity
 func (b *Ball) Update() {
 	b.pos.x += b.vel.x
 	b.pos.y += b.vel.y
 	b.tryBounceOffLimits()
 }
 
+// Bounce on collision with paddle
 func (b *Ball) Bounce(pLeft *Paddle, pRigth *Paddle) {
 
-	if int(b.pos.x)-b.radius < int(pLeft.pos.x)+pLeft.size.w/2 {
-		if int(b.pos.y) > int(pLeft.pos.y)-pLeft.size.h/2 && int(b.pos.y) < int(pLeft.pos.y)+pLeft.size.h/2 {
+	if int(b.pos.x-b.radius) < int(pLeft.pos.x+pLeft.size.w/2) {
+		if int(b.pos.y) > int(pLeft.pos.y-pLeft.size.h/2) && int(b.pos.y) < int(pLeft.pos.y+pLeft.size.h/2) {
 			b.vel.x *= -1
 		}
 	}
-	if int(b.pos.x)+b.radius > int(pRigth.pos.x)-pRigth.size.w/2 {
-		if int(b.pos.y) > int(pRigth.pos.y)-pRigth.size.h/2 && int(b.pos.y) < int(pRigth.pos.y)+pRigth.size.h/2 {
+	if int(b.pos.x+b.radius) > int(pRigth.pos.x-pRigth.size.w/2) {
+		if int(b.pos.y) > int(pRigth.pos.y-pRigth.size.h/2) && int(b.pos.y) < int(pRigth.pos.y+pRigth.size.h/2) {
 			b.vel.x *= -1
 		}
 	}
@@ -50,7 +53,8 @@ func (b *Ball) tryBounceOffLimits() {
 	}
 }
 
-func NewBall(p Position, r int, c Color, v Velocity) *Ball {
+// NewBall Initializes a ball
+func NewBall(p Position, r float32, c Color, v Velocity) *Ball {
 	return &Ball{
 		pos:    p,
 		radius: r,
@@ -69,25 +73,34 @@ type Paddle struct {
 // Draw implementation of Drawable for Paddle
 func (p *Paddle) Draw(pixels []byte) {
 	st := NewPosition(p.pos.x-float32(p.size.w)/2.0, p.pos.y-float32(p.size.h)/2.0)
-	for y := 0; y < p.size.h; y++ { //Iterate on Y first for best memory management
-		for x := 0; x < p.size.w; x++ {
+	for y := 0; y < int(p.size.h); y++ { //Iterate on Y first for best memory management
+		for x := 0; x < int(p.size.w); x++ {
 			setPixels(int(st.x)+x, int(st.y)+y, p.color, pixels)
 		}
 	}
 }
 
+// Update paddle position
 func (p *Paddle) Update(evt KeyboardEvent) {
-	if evt.Key == ArrowUp {
+	if evt.Key == ArrowUp && evt.Keydown > 0 {
 		p.pos.y -= 20
-	} else if evt.Key == ArrowDown {
+	} else if evt.Key == ArrowDown && evt.Keydown > 0 {
 		p.pos.y += 20
 	}
 }
 
+// AutoUpdate automatic paddle
 func (p *Paddle) AutoUpdate(b *Ball) {
-	p.pos.y = b.pos.y
+	// p.pos.y = b.pos.y
+	if p.pos.y > b.pos.y {
+		p.pos.y -= 20
+	}
+	if p.pos.y < b.pos.y {
+		p.pos.y += 20
+	}
 }
 
+// NewPaddle Initialize paddle
 func NewPaddle(p Position, s Size, c Color) *Paddle {
 	return &Paddle{
 		pos:   p,
@@ -96,6 +109,7 @@ func NewPaddle(p Position, s Size, c Color) *Paddle {
 	}
 }
 
+// Position represents a position on x and y axis
 type Position struct {
 	x, y float32
 }
@@ -105,25 +119,28 @@ func NewPosition(x, y float32) Position {
 	return Position{x, y}
 }
 
+// Velocity represents a velocity on x and y axis
 type Velocity struct {
 	x, y float32
 }
 
-// NewPosition Initialize position
+// NewVelocity Initialize velocity
 func NewVelocity(x, y float32) Velocity {
 	return Velocity{x, y}
 }
 
+// Color Represents a byte color using RGB
 type Color struct {
 	r, g, b byte
 }
 
+// Size represents a Height and Width
 type Size struct {
-	w, h int
+	w, h float32
 }
 
 // NewSize Initialize size
-func NewSize(w, h int) Size {
+func NewSize(w, h float32) Size {
 	return Size{w, h}
 }
 
@@ -144,5 +161,6 @@ const (
 )
 
 var (
+	// White RGB Color
 	White = Color{255, 255, 255}
 )
